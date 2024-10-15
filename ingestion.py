03c8e736f9b3d22f04d4c6ea1ad2a439fea6d15a
jackyjlxing <xingjl@hotmail.com>
2024-10-15 08:48:16 -0400

    RAG with chat history with pinecone vector DB and llama3.2

--- ingestion.py
+++ ingestion.py
@@ -1,34 +1,33 @@
 from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import ReadTheDocsLoader
+from langchain_ollama import OllamaEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
 
 load_dotenv()
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import ReadTheDocsLoader
-from langchain_openai import OpenAIEmbeddings
-from langchain_pinecone import PineconeVectorStore
-
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-
+embeddings = OllamaEmbeddings(model="llama3.2")
 
 def ingest_docs():
-    loader = ReadTheDocsLoader("langchain-docs/langchain.readthedocs.io/en/v0.1")
+    loader = ReadTheDocsLoader("langchain-docs/api.python.langchain.com/en/latest")
 
     raw_documents = loader.load()
     print(f"loaded {len(raw_documents)} documents")
 
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=600, chunk_overlap=50)
     documents = text_splitter.split_documents(raw_documents)
+
     for doc in documents:
-        new_url = doc.metadata["source"]
+        new_url = doc.metadata['source']
         new_url = new_url.replace("langchain-docs", "https:/")
         doc.metadata.update({"source": new_url})
 
     print(f"Going to add {len(documents)} to Pinecone")
-    PineconeVectorStore.from_documents(
-        documents, embeddings, index_name="langchain-doc-index"
-    )
-    print("****Loading to vectorstore done ***")
+    PineconeVectorStore.from_documents(documents,embeddings, index_name="langchain-doc-index")
 
+    print("****Loading to vectorstore done &***")
 
 if __name__ == "__main__":
+    print("ingestion")
     ingest_docs()
